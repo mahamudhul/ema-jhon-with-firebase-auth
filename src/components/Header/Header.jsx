82a6bddfa6ba8b1fe4a14babab49da62c1e4cc8f
mahamudhul <111944934+mahamudhul@users.noreply.github.com>
2023-04-28 22:31:49 +0600

    first commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,36 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './Header.css';
 import logo from '../../images/Logo.svg';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../providers/AuthProvider';
 
 const Header = () => {
+
+    const { user, logOut } = useContext(AuthContext)
+
+    const handleLogOut = () => {
+        logOut()
+            .then((result) => {
+
+            })
+            .catch((error) => {
+                console.error(error)
+            })
+    }
+
     return (
         <nav className='header'>
             <img src={logo} alt="" />
-            <div>
+            <div >
                 <Link to="/">Shop</Link>
                 <Link to="/orders">Orders</Link>
                 <Link to="/inventory">Inventory</Link>
                 <Link to="/login">Login</Link>
+                <Link to="/signup">Sign Up</Link>
+                <div>
+                    {user && <span className='user-id'>Welcome {user.email} <button onClick={handleLogOut}>Sign Out</button> </span>}
+                </div>
+
             </div>
         </nav>
     );
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCiruwtXNNu6yrbN6L8hfu5ZwyPuQ2GzXs",
+  authDomain: "ema-jhon-with-firebase-a-38539.firebaseapp.com",
+  projectId: "ema-jhon-with-firebase-a-38539",
+  storageBucket: "ema-jhon-with-firebase-a-38539.appspot.com",
+  messagingSenderId: "819758402392",
+  appId: "1:819758402392:web:5304b1ae0d06d5cd30732d"
+};
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+export default app;
